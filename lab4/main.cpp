8ae740af2a3deb83133841dcbbc8834e29fccaad
456i <timofeymarushko@gmail.com>
2024-09-14 23:13:55 +0300

    pass

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <Windows.h>
 using namespace std;
 
 
---
+++ lab5/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    bitset<4> bt;
+    cin >> num;
+    string str = to_string(num);
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (bt[str[i] - '0'])
+        {
+            cout << "Цифры не различны" << endl;
+            return 0;
+        }
+        else
+        {
+            bt[str[i] - '0'] = 1;
+        }
+    }
+    cout << "Цифры различны" << endl;
+    return 0;
+}
