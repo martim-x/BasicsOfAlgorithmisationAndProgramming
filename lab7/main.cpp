4443b6c43dff72308e05855dafc4564b98e56f46
456i <timofeymarushko@gmail.com>
2024-09-26 14:18:24 +0300

    pass

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,134 +1,144 @@
 #include <bits/stdc++.h>
+#include <Windows.h>
 using namespace std;
 // chcp 65001 For Windows
 // export LANG=en_US.UTF-8 For Linux
 
+
 vector<long long> searchPrimes(long long num) {
-  bitset<10000000> bs;
-  bs.set();
+    bitset<10000000> bs;
+    bs.set();
 
-  vector<long long> primes;
+    vector<long long> primes;
 
-  long long sqrtOfnum = sqrt(num);
-  for (long long i = 2; i <= sqrtOfnum; i++)
-    if (bs[i]) {
-      for (long long j = i; j < sqrtOfnum; j += i) {
-        bs[j] = 0;
-      }
-      primes.push_back(i);
-    }
-  return primes;
+    long long sqrtOfnum = sqrt(num);
+    for (long long i = 2; i <= sqrtOfnum; i++)
+        if (bs[i]) {
+            for (long long j = i; j < sqrtOfnum; j += i)
+            {
+                bs[j] = 0;
+            }
+            primes.push_back(i);
+        }
+    return primes;
 }
+
 
 set<long long> searchDeviders(vector<long long> primes, long long num) {
-  set<long long> deviders;
-  for (auto dev : primes)
-    while (num % dev == 0) {
-      num /= dev;
-      deviders.insert(dev);
-    }
+    set<long long> deviders;
+    for (auto dev : primes)
+        while (num % dev == 0) {
+            num /= dev;
+            deviders.insert(dev);
+        }
 
-  for (auto dev : deviders)
-    cout << dev << endl;
-  return deviders;
+    for (auto dev : deviders)
+        cout << dev << endl;
+    return deviders;
 }
+
 
 void task8_3() {
-  double C1, C2;
-  cout << "task8_3" << endl;
-  cout << "Введите C1 и C2: " << endl;
-  cin >> C1 >> C2;
-  for (int i = 0; i < 12; i++) {
-    C2 += C1 / 2;
-    C1 = C1 / 2;
-    C1 += C2 / 2;
-    C2 = C2 / 2;
-  }
-  cout << "C1 = " << C1 << ", C2 = " << C2 << endl;
+    double C1, C2;
+    cout << "task8_3" << endl;
+    cout << "Введите C1 и C2: " << endl;
+    cin >> C1 >> C2;
+    for (int i = 0; i < 12; i++) {
+        C2 += C1 / 2;
+        C1 = C1 / 2;
+        C1 += C2 / 2;
+        C2 = C2 / 2;
+    }
+    cout << "C1 = " << C1 << ", C2 = " << C2 << endl;
 }
+
 
 void task8_1() {
-  vector<long long> ultraprimes = searchPrimes(999);
+    vector<long long> ultraprimes = searchPrimes(999);
 
-  vector<long long> megaultraprimes;
+    vector<long long> megaultraprimes;
 
-  ultraprimes.erase(ultraprimes.begin(), ultraprimes.begin() + 4);
+    ultraprimes.erase(ultraprimes.begin(), ultraprimes.begin() + 4);
 
-  string strPrime;
-  for (auto prime : ultraprimes) {
-    strPrime = to_string(prime * prime);
-    if (strPrime[0] - '0' < strPrime[1] - '0' &&
-        strPrime[2] - '0' < strPrime[1] - '0') {
-      megaultraprimes.push_back(prime);
+    string strPrime;
+    for (auto prime : ultraprimes) {
+        strPrime = to_string(prime * prime);
+        if (strPrime[0] - '0' < strPrime[1] - '0' && strPrime[2] - '0' < strPrime[1] - '0')
+            megaultraprimes.push_back(prime);
     }
-  }
-  cout << "task 8_1  ultraprimes" << endl;
-  for (auto prime : megaultraprimes) {
-    cout << prime << endl;
-  }
+    cout << "task 8_1  ultraprimes" << endl;
+
+    for (auto prime : megaultraprimes) {
+        cout << prime << endl;
+    }
 }
 
-void subtask8_2(set<pair<int, vector<int>>> &summary,
-                set<pair<int, vector<int>>> &buffers, vector<int> coins,
-                bool executable) {
-  for (auto sum : summary) {
-    for (auto coin : coins) {
-      if (sum.first >= coin || sum.first == 0) {
-        vector<int> newCombination = sum.second;
-        pair<int, vector<int>> pairForBuffers;
 
-        if (sum.first) {
-          newCombination.push_back(coin);
-          sort(newCombination.begin(), newCombination.end());
-          pairForBuffers = make_pair(sum.first - coin, newCombination);
-          executable = true;
-        }
+void subtask8_2(set<pair<int, vector<int> > >& summary, set<pair<int, vector<int> > >& buffers, vector<int> coins, bool executable) {
+    for (auto sum : summary) {
 
-        else {
-          pairForBuffers = make_pair(sum.first, newCombination);
+        for (auto coin : coins) {
+
+            if (sum.first >= coin || sum.first == 0) {
+                vector<int> newCombination = sum.second;
+                pair<int, vector<int> > pairForBuffers;
+
+                if (sum.first) {
+                    newCombination.push_back(coin);
+                    sort(newCombination.begin(), newCombination.end());
+                    pairForBuffers = make_pair(sum.first - coin, newCombination);
+                    executable = true;
+                }
+
+                else {
+                    pairForBuffers = make_pair(sum.first, newCombination);
+                }
+                buffers.insert(pairForBuffers);
+            }
         }
-        buffers.insert(pairForBuffers);
-      }
     }
-  }
 
-  if (executable) {
-    summary.clear();
-    subtask8_2(buffers, summary, coins, false);
-  }
+    if (executable) {
+        summary.clear();
+        subtask8_2(buffers, summary, coins, false);
+    }
 }
 
 void task8_2() {
-  vector<int> coins = {50, 20, 5, 2};
-  set<pair<int, vector<int>>> buffers;
-  set<pair<int, vector<int>>> summary;
+    vector<int> coins = { 50, 20, 5, 2 };
+    set<pair<int, vector<int> > > buffers;
+    set<pair<int, vector<int> > > summary;
 
-  summary.insert(make_pair(100, vector<int>()));
+    summary.insert(make_pair(100, vector<int>()));
 
-  subtask8_2(summary, buffers, coins, false);
+    subtask8_2(summary, buffers, coins, false);
 
-  cout << "Amount of cases: " << buffers.size() << endl;
-  for (auto el : buffers) {
-    cout << el.first << ": ";
-    for (auto coin : el.second) {
-      cout << coin << " ";
+    cout << "Amount of cases: " << buffers.size() << endl;
+    for (auto el : buffers) {
+        cout << el.first << ": ";
+        for (auto coin : el.second) {
+            cout << coin << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
 
 int main() {
-  long long num;
-  cout << "Введите число: " << endl;
-  cin >> num;
+    //system("chcp 65001");
+    system("export LANG=en_US.UTF-8");
+    // SetConsoleCP(65001);
+    // SetConsoleOutputCP(65001);
+    long long num;
+    cout << "Введите число: " << endl;
+    cin >> num;
 
-  vector<long long> primes = searchPrimes(num);
-  set<long long> deviders = searchDeviders(primes, num);
+    vector<long long> primes = searchPrimes(num);
+    set<long long> deviders = searchDeviders(primes, num);
 
-  task8_3();
+    task8_3();
 
-  task8_1();
+    task8_1();
 
-  task8_2();
-  return 0;
+    task8_2();
+    return 0;
 }
