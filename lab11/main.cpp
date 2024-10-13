d006f2a2354da7db05777bc930b86793a2df2cbe
456i <timofeymarushko@gmail.com>
2024-10-13 13:19:40 +0300

    pass

--- lab11/main.cpp
+++ lab11/main.cpp
@@ -2,19 +2,34 @@
 using namespace std;
 
 
-void task5() {
+void task5_1() {
     int num;
     cout << "Input number: " << endl;
     cin >> num;
-    bitset<64> bt(num);
+    num = INT_MAX;
+    bitset<32> bt(num);
     cout << bt << endl;
     for (int i = 0; i < 32; i++) {
         if ((i + 1) % 3 == 0)
             num &= ~(1 << i);
     }
-    bitset<64> bts(num);
-    cout<<
+    bitset<32> bts(num);
+    cout << bts << endl;
+}
 
+
+
+void task5_2() {
+    int numA, numB, n;
+    cout << "Input number A: " << endl;
+    cin >> numA;
+    cout << "Input n: " << endl;
+    cin >> n;
+    bitset<32> bt(numA);
+    cout << bt << endl;
+    for (int i = 0; i < 3; i++) {
+        numA &= ~(1 << n + i);
+    }
 }
 
 
---
+++ lab12/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string input;
+    vector<vector<int>> matrix;
+    vector<int> buffer;
+    int num;
+    while(!EOF){
+        cin>>num;
+        buffer.push_back(num);
+    }
+    return 0;
+}
