50a3f188b1d21c5ca0e892869ae57d8c7ab0bbd0
456i <timofeymarushko@gmail.com>
2024-10-27 21:23:14 +0300

    + all tests passed

--- lab13/main.cpp
+++ lab13/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-void task6() {
+void task4() {
 
     char s1[] = "asdfghjkksfddsaf\0";
     char s2[] = "asdghhjhgfjkdasa\0";
@@ -32,7 +32,102 @@ void task6() {
     cout << "max symbols: " << *(--symbols.end()) << endl;
 }
 
+
+void task5_1() {
+    char str[] = "The Ugly Duckling is one of the most beloved English stories of all time.\n"
+        "The story follows the journey of an ugly duckling who is rejected by his family and friends.\n"
+        "He eventually learns to love himself for who he is and finds happiness in his new life.\n"
+        "The story is popular with English learners because it teaches valuable lessons about self-acceptance and diversity.\n"
+        "In addition, the story is written in simple, easy-to-understand English, making it an ideal choice for beginners.\n"
+        "Whether you're just starting to learn English or you’re a seasoned speaker, The Ugly Duckling is a great choice for anyone looking to improve their English skills.\0";
+
+    cout << "Current string:\n" << str << endl;
+
+    char term[50];
+    cout << "Input the terminate: ";
+    cin >> term;
+
+    int term_len = strlen(term);
+
+    char* word = strtok(str, " ,.;:\n");
+
+    while (word != nullptr) {
+        if (!strcmp(word + strlen(word) - term_len, term)) {
+            cout << word << endl;
+        }
+
+        word = strtok(nullptr, " ,.;:\n");
+    }
+}
+
+
+void task5_4() {
+    char str1[] = "The story follows the journey of an ugly duckling who is rejected by his family and friends.";
+    char* word1 = strtok(str1, " .,:;\n\0");
+    vector<char*> vecstr1;
+    while (word1 != nullptr) {
+        vecstr1.push_back(word1);
+        word1 = strtok(nullptr, " .,:;\n\0");
+    }
+
+    vector<string> vecstr2;
+    for (int i = 0; i < vecstr1.size(); i++)
+        if (i % 2 == 1)
+            vecstr2.push_back(string(vecstr1[i]));
+
+    for (string& i : vecstr2)
+        reverse(i.begin(), i.end());
+
+    for (auto i : vecstr2)
+        cout << i << " ";
+}
+
+
+void task5_5() {
+    char str1[] = "The story follows the journey of an ugly duckling who is rejected by his family and friends.";
+    char str2[] = "In of addition, the story is written by in simple, easy-to-understand English, making it an ideal choice for beginners.";
+
+    vector<char*> vecstr1;
+    vector<char*> vecstr2;
+
+    char* word1 = strtok(str1, " .,:;\n\0");
+    while (word1 != nullptr) {
+        vecstr1.push_back(word1);
+        word1 = strtok(nullptr, " .,:;\n\0");
+    }
+
+    char* word2 = strtok(str2, " .,:;\n\0");
+    while (word2 != nullptr) {
+        vecstr2.push_back(word2);
+        word2 = strtok(nullptr, " .,:;\n\0");
+    }
+
+    char* sh_word = vecstr1[0];
+    bool flag = false;
+    for (auto word1 : vecstr1) {
+        if (strlen(sh_word) > strlen(word1) || flag) {
+            strcpy(sh_word, word1);
+            flag = false;
+        }
+
+        for (auto word2 : vecstr2)
+        {
+            cout << word1 << " " << word2 << endl;
+            if (!strcmp(word1, word2)) {
+                flag = true;
+                break;;
+            }
+        }
+
+    }
+    cout << "shortest word: " << sh_word << endl;
+}
+
+
 int main() {
-    task6();
+    task4();
+    task5_1();
+    task5_4();
+    task5_5();
     return 0;
 }
