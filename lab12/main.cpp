af9162f020aaf735d4b4e5a59834a14b677c1864
456i <timofeymarushko@gmail.com>
2024-10-27 13:46:17 +0300

    pass

--- lab12/main.cpp
+++ lab12/main.cpp
@@ -7,19 +7,20 @@ void task5_1() {
     int k, n, buffer;
     cout << "Input k and n elements: " << endl;
     cin >> k >> n;
-    vector<int> X, Y, Z;
+    vector<int> X, Y;
+    set<int> Z;
     srand(time(0));
 
     cout << "\nMassive X" << endl;
     for (int i = 0; i < k; i++) {
-        buffer = rand() % 21 - 10;
+        buffer = rand() % 201 - 100;
         X.push_back(buffer);
         cout << buffer << endl;
     }
 
     cout << "\nMassive Y" << endl;
     for (int i = 0; i < n; i++) {
-        buffer = rand() % 21 - 10;
+        buffer = rand() % 201 - 100;
         Y.push_back(buffer);
         cout << buffer << endl;
     }
@@ -28,7 +29,7 @@ void task5_1() {
     for (auto elX : X)
         for (auto elY : Y)
             if (elX == elY) {
-                Z.push_back(elX);
+                Z.insert(elX);
                 cout << elX << endl;
             }
 }
@@ -47,27 +48,25 @@ void task5_2() {
 
     cout << "\nMassive x" << endl;
     for (int i = 0; i < k; i++) {
-        buffer = rand() % 21 - 10;
+        buffer = rand() % 201 - 100;
         x.push_back(buffer);
         cout << buffer << endl;
     }
 
     cout << "\nMassive y" << endl;
     for (int i = 0; i < n; i++) {
-        buffer = rand() % 21 - 10;
+        buffer = rand() % 201 - 100;
         y.push_back(buffer);
         cout << buffer << endl;
     }
 
     for (auto i : x)
         for (auto j : y)
-            if (i + j != q)
-                summary.insert(i + j);
+            summary.insert(i + j);
 
     cout << "\nSet" << endl;
     for (auto el : summary)
         cout << el << endl;
-
 
     auto it = summary.lower_bound(q);
     auto lastIt = summary.end();
@@ -76,12 +75,16 @@ void task5_2() {
     if (it == summary.end())
         cout << *(--it) << endl;
     else
-        if (abs(q - *it) < abs(q - *(--it))) {
+        if (it == summary.begin())
             cout << *(++it) << endl;
-        }
-        else {
-            cout << *it << endl;
-        }
+
+        else
+            if (abs(q - *it) < abs(q - *(--it))) {
+                cout << *(++it) << endl;
+            }
+            else {
+                cout << *it << endl;
+            }
 }
 
 
@@ -90,20 +93,21 @@ void task12_1AND12_2() {
     int n, numA, numB, counter_eq = 0, counter_gr = 0, counter_lw = 0;
     vector<int> vecA, vecB;
     cout << "Input n: " << endl;
+    cin >> n;
     srand(time(0));
-    for (int i = 0;i < n;i++) {
-        vecA.push_back(rand() % 100);
-        vecB.push_back(rand() % 100);
+    for (int i = 0; i < n; i++) {
+        vecA.push_back(rand() % 101);
+        vecB.push_back(rand() % 101);
     }
 
-    for (int i = 0;i < n;i++) {
+    for (int i = 0; i < n; i++) {
         numA = vecA[i];
         numB = vecB[i];
-        if (numA = numB)
+        if (numA == numB)
             counter_eq++;
         if (numA > numB)
             counter_gr++;
-        if (numA = numB)
+        if (numA < numB)
             counter_lw++;
     }
 
@@ -113,14 +117,34 @@ void task12_1AND12_2() {
 
     int k;
     cout << "Input k: " << endl;
-    vector<int> A;
-    for (int i = 0;i < 100;i++) {
-        numA = rand() % 100;
-    }
+    cin >> k;
+
+    vector<int> A, GR, LW, EQ;
 
     for (int i = 0; i < 100; i++)
+        A.push_back(rand() % 201 - 100);
+
+    for (int i = 0; i < 100; i++) {
         if (A[i] < k)
-            cout << "A[" << i << "] " << A[i] << " < " << k << endl;
+            LW.push_back(i);
+
+        if (A[i] == k)
+            EQ.push_back(i);
+
+        if (A[i] > k)
+            GR.push_back(i);
+    }
+    cout << "Index which is lower than k: " << k << endl;
+    for (auto i : LW)
+        cout << i << endl;
+
+    cout << "Index which is equal k: " << k << endl;
+    for (auto i : EQ)
+        cout << i << endl;
+
+    cout << "Index which is greater than k: " << k << endl;
+    for (auto i : GR)
+        cout << i << endl;
 }
 
 
@@ -129,8 +153,8 @@ void task14_1AND14_2() {
     int i, j, index = 0, counterPairs = 0;
     vector<int> vecA, vecB;
     srand(time(0));
-    i = rand() % 100;
-    j = rand() % 100;
+    cout << "Input i and j: " << endl;
+    cin >> i >> j;
 
     for (; index < i; index++)
         vecA.push_back(rand() % 100);
@@ -141,10 +165,12 @@ void task14_1AND14_2() {
         vecB.push_back(rand() % 100);
 
     for (auto elA : vecA)
-        for (auto elB : vecB)
+        for (auto elB : vecB) {
+            cout << "elA: " << elA << ", elB: " << elB << endl;
             if (elA == elB)
                 counterPairs++;
-
+        }
+    cout << "Equal pairs: " << endl;
     sort(vecA.begin(), vecA.end());
     for (auto el : vecA)
         cout << el << " ";
@@ -153,33 +179,56 @@ void task14_1AND14_2() {
 
 
 void task15_1AND15_2() {
-    cout << "task14_1AND14_2" << endl;
-    int a, c;
-    c = 5;
-    a = c;
-    cout << "c: " << c << endl;
-    cout << "a: " << a << endl;
-    c--;
-    cout << "c: " << c << endl;
-    cout << "a: " << a << endl;
+    cout << "task15_1AND15_2" << endl;
+    vector<int> A;
+    int n, num;
+    cout << "Input size n: " << endl;
+    cin >> n;
+    srand(time(0));
+    for (int i = 0; i < n; i++) {
+        num = rand() % 201 - 100;
+        if (!num) {
+            num++;
+        }
+        A.push_back(num);
+    }
+    sort(A.begin(), A.end());
+    cout << "Elements in A" << endl;
+    for (int i : A)
+        cout << i << endl;
+    cout << endl;
 
-    int &b = c;
-    cout << "c: " << c << endl;
-    cout << "a: " << a << endl;
-    c--;
-    cout << "c: " << c << endl;
-    cout << "a: " << a << endl;
 
+    vector<int> F, D;
+    for (int i = 0; i < 100; i++) {
+        num = rand() % 201 - 100;
+        if (!num) {
+            num++;
+        }
+        F.push_back(num);
 
+        num = rand() % 201 - 100;
+        if (!num) {
+            num++;
+        }
+        D.push_back(num);
+    }
+
+    int maxElF = *max_element(F.begin(), F.end());
+    int maxElD = *max_element(D.begin(), D.end());
+    if (maxElD == maxElF)
+        cout << "Element in F: " << maxElF << " exists in D" << endl;
+    else
+        cout << "Element in F: " << maxElF << " doesn't exists in D" << endl;
 
 }
 
 
 int main() {
-    // task5_1();
-    // task5_2();
-    // task12_1AND12_2();
-    // task14_1AND14_2();
+    task5_1();
+    task5_2();
+    task12_1AND12_2();
+    task14_1AND14_2();
     task15_1AND15_2();
     return 0;
 }
