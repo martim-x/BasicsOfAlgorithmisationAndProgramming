d4f27e2eb0ade17263f3f1bc5bab4a9ee17455a8
456i <timofeymarushko@gmail.com>
2024-10-29 21:52:58 +0300

    finaly made lab13

--- lab13/main.cpp
+++ lab13/main.cpp
@@ -16,33 +16,13 @@ bool OverRidestrcmp(char str1[], char str2[]) {
         return false;
 
     while (str1[i] != '\0') {
-        if (str1[i] != str2[2])
+        if (str1[i] != str2[i])
             return false;
         i++;
     }
 
     return true;
 }
-
-// OVERDRIVE!!!
-// void OverRidestrcpy(char* dest[], char* src[]) {
-//     int i = 0;
-//     while (src[i] != '\0') {
-//         dest[i] = src[i];
-//         i++;
-//     }
-//     dest[i] = '\0';
-// }
-
-
-// void OverRidestrcpy(const char* dest[],const char* src[]) {
-//     int i = 0;
-//     while (src[i] != '\0') {
-//         dest[i] = src[i];
-//         i++;
-//     }
-//     dest[i] = '\0';
-// }
 
 
 void OverRidestrcpy(char dest[], char src[]) {
@@ -53,72 +33,6 @@ void OverRidestrcpy(char dest[], char src[]) {
     }
     dest[i] = '\0';
 }
-
-
-
-// vector<const char*> OverRidestrtok(const char* str[],const  char* tokens[]) {
-//     int i = 0, j = 0;
-//     vector<const char*> tokenedVec;
-//     int pointerToken = 0;
-
-//     const char* strSave[OverRidestrlen(str) + 1];
-//     OverRidestrcpy(strSave, str);
-
-//     while (str[i] != '\0') {
-//         j = 0;
-//         while (tokens[j] != '\0') {
-//             if (str[i] == tokens[j]) {
-//                 str[i] = '\0';
-//                 tokenedVec.push_back(&str[pointerToken]);
-//                 pointerToken = i + 1;
-//                 break;
-//             }
-//             j++;
-//         }
-//         i++;
-//     }
-
-//     if (pointerToken < i)
-//         tokenedVec.push_back(&str[pointerToken]);
-
-//     OverRidestrcpy(str, strSave);
-
-//     return tokenedVec;
-
-// }
-
-
-
-// vector<const char*> OverRidestrtok(char str[], char tokens[]) {
-//     int i = 0, j = 0;
-//     vector<const char*> tokenedVec;
-//     int pointerToken = 0;
-
-//     const char* strSave[OverRidestrlen(str) + 1];
-//     OverRidestrcpy(strSave, str);
-
-//     while (str[i] != '\0') {
-//         j = 0;
-//         while (tokens[j] != '\0') {
-//             if (str[i] == tokens[j]) {
-//                 str[i] = '\0';
-//                 tokenedVec.push_back(&str[pointerToken]);
-//                 pointerToken = i + 1;
-//                 break;
-//             }
-//             j++;
-//         }
-//         i++;
-//     }
-
-//     if (pointerToken < i)
-//         tokenedVec.push_back(&str[pointerToken]);
-
-//     OverRidestrcpy(str, strSave);
-
-//     return tokenedVec;
-
-// }
 
 
 vector<char*> OverRidestrtok(char str[], char tokens[]) {
@@ -134,7 +48,12 @@ vector<char*> OverRidestrtok(char str[], char tokens[]) {
         while (tokens[j] != '\0') {
             if (str[i] == tokens[j]) {
                 str[i] = '\0';
-                tokenedVec.push_back(&str[pointerToken]);
+                if (pointerToken != i) {
+                    char* bufferchar = new char[OverRidestrlen(str + pointerToken) + 1];
+                    OverRidestrcpy(bufferchar, str + pointerToken);
+
+                    tokenedVec.push_back(bufferchar);
+                }
                 pointerToken = i + 1;
                 break;
             }
@@ -143,8 +62,11 @@ vector<char*> OverRidestrtok(char str[], char tokens[]) {
         i++;
     }
 
-    if (pointerToken < i)
-        tokenedVec.push_back(&str[pointerToken]);
+    if (pointerToken < i) {
+        char* bufferchar = new char[OverRidestrlen(str + pointerToken) + 1];
+        OverRidestrcpy(bufferchar, str + pointerToken);
+        tokenedVec.push_back(bufferchar);
+    }
 
     OverRidestrcpy(str, strSave);
 
@@ -158,15 +80,18 @@ void OverRidestrev(char word[]) {
     char wordBuf[leghtStr];
     OverRidestrcpy(wordBuf, word);
     int i = 0;
-    while (i <= leghtStr) {
-        word[i] = wordBuf[leghtStr - i];
+    while (i < leghtStr) {
+        word[i] = wordBuf[leghtStr - i - 1];
         i++;
     }
+    word[i] = '\0';
 }
 
 
 void task4() {
-
+    cout << endl;
+    cout << "task4" << endl;
+    cout << endl;
     char s1[] = "asdfghjkksfddsaf\0";
     char s2[] = "asdghhjhgfjkdasa\0";
     char c1, c2;
@@ -175,7 +100,7 @@ void task4() {
         c1 = s1[i];
         c2 = s2[i];
         if (c2 == c1)
-            cout << "c2 equals c1        -- " << "c2: " << c2 << ", c1: " << c1 << endl;
+            cout << "c2 equals c1 --------- " << "c2: " << c2 << ", c1: " << c1 << endl;
         else
             cout << "c2 doesm't equal c1 -- " << "c2: " << c2 << ", c1: " << c1 << endl;
     }
@@ -199,12 +124,15 @@ void task4() {
 
 
 void task5_1() {
+    cout << endl;
+    cout << "task4" << endl;
+    cout << endl;
     char str[] = "The Ugly Duckling is one of the most beloved English stories of all time.\n"
         "The story follows the journey of an ugly duckling who is rejected by his family and friends.\n"
         "He eventually learns to love himself for who he is and finds happiness in his new life.\n"
         "The story is popular with English learners because it teaches valuable lessons about self-acceptance and diversity.\n"
         "In addition, the story is written in simple, easy-to-understand English, making it an ideal choice for beginners.\n"
-        "Whether you're just starting to learn English or you’re a seasoned speaker, The Ugly Duckling is a great choice for anyone looking to improve their English skills.\0";
+        "Whether you're just starting to learn English or you're a seasoned speaker, The Ugly Duckling is a great choice for anyone looking to improve their English skills.\0";
 
     cout << "Current string:\n" << str << endl;
 
@@ -217,61 +145,54 @@ void task5_1() {
     char terminators[] = " .;:\n\0";
     vector<char*> words = OverRidestrtok(str, terminators);
 
-    for (auto word : words) {
-        cout << word << endl;
+    for (auto word : words)
         if (OverRidestrcmp(word + OverRidestrlen(word) - term_len, term))
             cout << word << endl;
-    }
 }
 
 
 void task5_4() {
+    cout << endl;
+    cout << "task4" << endl;
+    cout << endl;
     char str1[] = "The story follows the journey of an ugly duckling who is rejected by his family and friends.";
     char terminators[] = " .;:\n\0";
     vector<char*> words = OverRidestrtok(str1, terminators);
     vector<char*> wordsODD;
     for (int i = 0; i < words.size(); i++)
-        if (i % 2)
-            wordsODD.push_back(words[i]);
-
-    for (auto i : wordsODD)
-        cout << i << " ";
+        if (i % 2) {
+            OverRidestrev(words[i]);
+            cout << words[i] << " ";
+        }
+    cout << endl;
 }
 
 
 void task5_5() {
+    cout << endl;
+    cout << "task4" << endl;
+    cout << endl;
     char str1[] = "The story follows the journey of an ugly duckling who is rejected by his family and friends.";
     char str2[] = "In of addition, the story is written by in simple, easy-to-understand English, making it an ideal choice for beginners.";
 
-    vector<char*> vecstr1;
-    vector<char*> vecstr2;
+    char terminators[] = " .,:;\n\0";
 
-    char* word1 = strtok(str1, " .,:;\n\0");
-    while (word1 != nullptr) {
-        vecstr1.push_back(word1);
-        word1 = strtok(nullptr, " .,:;\n\0");
-    }
-
-    char* word2 = strtok(str2, " .,:;\n\0");
-    while (word2 != nullptr) {
-        vecstr2.push_back(word2);
-        word2 = strtok(nullptr, " .,:;\n\0");
-    }
+    vector<char*> vecstr1 = OverRidestrtok(str1, terminators);
+    vector<char*> vecstr2 = OverRidestrtok(str2, terminators);
 
     char* sh_word = vecstr1[0];
     bool flag = false;
     for (auto word1 : vecstr1) {
-        if (strlen(sh_word) > strlen(word1) || flag) {
-            strcpy(sh_word, word1);
+        if (OverRidestrlen(sh_word) > OverRidestrlen(word1) || flag) {
+            OverRidestrcpy(sh_word, word1);
             flag = false;
         }
 
-        for (auto word2 : vecstr2)
-        {
-            cout << word1 << " " << word2 << endl;
-            if (!strcmp(word1, word2)) {
+        for (auto word2 : vecstr2) {
+            // cout << sh_word << " " << word1 << " " << word2 << endl;
+            if (OverRidestrcmp(word1, word2)) {
                 flag = true;
-                break;;
+                break;
             }
         }
 
@@ -281,9 +202,9 @@ void task5_5() {
 
 
 int main() {
-    // task4();
+    task4();
     task5_1();
-    // task5_4();
-    // task5_5();
+    task5_4();
+    task5_5();
     return 0;
 }
