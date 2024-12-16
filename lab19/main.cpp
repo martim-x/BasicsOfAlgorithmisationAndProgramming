8220955e37f23554e9907c624f8e685e27694206
tim_456i <127132269+456i@users.noreply.github.com>
2024-12-16 15:56:52 +0300

    pass

--- lab19/main.cpp
+++ lab19/main.cpp
@@ -1,120 +1,121 @@
 #include <iostream>
-#include <fstream>
-#include <set>
 #include <vector>
-
+#include <set>
+#include <string>
+#include <cstdio>
+#include <cstdlib> // Для exit
 using namespace std;
 
-
 void task8_1() {
-    ifstream inputFile("tests/input8_1.txt");
+    FILE* inputFile = fopen("tests/input8_1.txt", "r");
 
     vector<int> nums;
     int num;
 
-    while (inputFile >> num)
+    while (fscanf(inputFile, "%d", &num) == 1)
         nums.push_back(num);
 
-    inputFile.close();
+    fclose(inputFile);
 
-    ofstream outputFile("tests/input8_1.txt");
+    FILE* outputFile = fopen("tests/input8_1.txt", "w");
 
     int n = nums.size();
     for (int i = 0; i < (n + 1) / 2; i++) {
-        outputFile << nums[i] << " ";
+        fprintf(outputFile, "%d ", nums[i]);
         if (i != n - 1 - i)
-            outputFile << nums[n - 1 - i] << " ";
-
+            fprintf(outputFile, "%d ", nums[n - 1 - i]);
     }
-    outputFile.close();
+
+    fclose(outputFile);
 }
 
 
 void task8_2() {
-    ifstream inputFileA("tests/input8_2A.txt");
-    ifstream inputFileB("tests/input8_2B.txt");
-    ofstream outputFile("tests/output8_2.txt");
+    FILE* inputA = fopen("tests/input8_2A.txt", "r");
+    FILE* inputB = fopen("tests/input8_2B.txt", "r");
+    FILE* outputFile = fopen("tests/output8_2.txt", "w");
 
-    int n, buffer_num;
-    inputFileA >> n;
+    int n;
+    fscanf(inputA, "%d", &n);
+
     vector<vector<int>> matrixA(n, vector<int>(n));
     vector<int> matrixB(n);
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            inputFileA >> matrixA[i][j];
-
-    inputFileA.close();
+            fscanf(inputA, "%d", &matrixA[i][j]);
 
     for (int i = 0; i < n; i++)
-        inputFileB >> matrixB[i];
-
-    inputFileB.close();
+        fscanf(inputB, "%d", &matrixB[i]);
 
     for (int i = 0; i < n; i++) {
         int sum = 0;
         for (int j = 0; j < n; j++)
             sum += matrixA[i][j] * matrixB[j];
 
-        outputFile << sum << " ";
+        fprintf(outputFile, "%d ", sum);
     }
-    outputFile.close();
+
+    fclose(inputA);
+    fclose(inputB);
+    fclose(outputFile);
 }
 
 
 void task11_1() {
-    ifstream inputFile("tests/input11_1.txt");
-    ofstream outputFile("tests/output11_1.txt");
+    FILE* inputFile = fopen("tests/input11_1.txt", "r");
+    FILE* outputFile = fopen("tests/output11_1.txt", "w");
 
     set<int> numbers;
     int num;
 
-    while (inputFile >> num)
+    while (fscanf(inputFile, "%d", &num) == 1)
         numbers.insert(num);
 
-    inputFile.close();
+    for (const auto& val : numbers)
+        fprintf(outputFile, "%d ", val);
 
-    while (!numbers.empty()) {
-        outputFile << *numbers.begin() << " ";
-        numbers.erase(numbers.begin());
-    }
-    outputFile.close();
+    fclose(inputFile);
+    fclose(outputFile);
 }
 
 
 void task11_2() {
-    ifstream inputFile("tests/input11_2.txt");
-    ofstream outputFile("tests/output11_2.txt");
-    vector<string> terminatedStrings;
-    string line;
+    FILE* inputFile = fopen("tests/input11_2.txt", "r");
+    FILE* outputFile = fopen("tests/output11_2.txt", "w");
 
-    while (getline(inputFile, line))
+    vector<string> terminatedStrings;
+    char buffer[256];
+
+    while (fgets(buffer, sizeof(buffer), inputFile)) {
+        string line(buffer);
+        if (!line.empty() && line.back() == '\n')
+            line.pop_back();
         if (!line.empty() && line.back() == 'a')
             terminatedStrings.push_back(line);
+    }
 
-    inputFile.close();
+    for (const auto& str : terminatedStrings)
+        fprintf(outputFile, "%s\n", str.c_str());
 
-    for (auto str : terminatedStrings)
-        outputFile << str << endl;
-
-    outputFile.close();
+    fclose(inputFile);
+    fclose(outputFile);
 }
 
 
 void task14_1() {
-    ifstream inputFile("tests/input14_1.txt");
-    ofstream outputFile("tests/output14_1.txt");
+    FILE* inputFile = fopen("tests/input14_1.txt", "r");
+    FILE* outputFile = fopen("tests/output14_1.txt", "w");
 
     int k, column;
-    inputFile >> k;
-
+    fscanf(inputFile, "%d", &k);
     vector<vector<int>> matrix(k, vector<int>(k));
 
     for (int i = 0; i < k; i++)
         for (int j = 0; j < k; j++)
-            inputFile >> matrix[i][j];
+            fscanf(inputFile, "%d", &matrix[i][j]);
 
-    inputFile.close();
+    fclose(inputFile);
 
     while (true) {
         cout << "Enter the column number: ";
@@ -126,107 +127,101 @@ void task14_1() {
     }
 
     for (int i = 0; i < k; i++)
-        outputFile << matrix[i][column - 1] << endl;
+        fprintf(outputFile, "%d\n", matrix[i][column - 1]);
 
-    outputFile.close();
+    fclose(outputFile);
 }
 
 
 void task14_2() {
-    ifstream inputFile("tests/input14_2.txt");
-    ofstream outputFile("tests/output14_2.txt");
+    FILE* inputFile = fopen("tests/input14_2.txt", "r");
+    FILE* outputFile = fopen("tests/output14_2.txt", "w");
 
     set<int> numbers;
     int num;
 
-    while (inputFile >> num)
+    while (fscanf(inputFile, "%d", &num) == 1)
         numbers.insert(num);
 
-    inputFile.close();
+    for (const auto& val : numbers)
+        fprintf(outputFile, "%d ", val);
 
-    while (!numbers.empty()) {
-        outputFile << *numbers.begin() << " ";
-        numbers.erase(numbers.begin());
-    }
-    outputFile.close();
+    fclose(inputFile);
+    fclose(outputFile);
 }
 
 
 void task1_1() {
-    ifstream inputFileA("tests/input1_1A.txt");
-    ifstream inputFileB("tests/input1_1B.txt");
-    ofstream outputFile("tests/output1_1.txt");
+    FILE* inputA = fopen("tests/input1_1A.txt", "r");
+    FILE* inputB = fopen("tests/input1_1B.txt", "r");
+    FILE* outputFile = fopen("tests/output1_1.txt", "w");
 
     int rowsA, columnsA, rowsB, columnsB;
 
-    inputFileA >> rowsA >> columnsA;
+    fscanf(inputA, "%d%d", &rowsA, &columnsA);
     vector<vector<int>> matrixA(rowsA, vector<int>(columnsA));
 
     for (int i = 0; i < rowsA; i++)
         for (int j = 0; j < columnsA; j++)
-            inputFileA >> matrixA[i][j];
+            fscanf(inputA, "%d", &matrixA[i][j]);
 
-    inputFileB >> rowsB >> columnsB;
+    fscanf(inputB, "%d%d", &rowsB, &columnsB);
     vector<vector<int>> matrixB(rowsB, vector<int>(columnsB));
 
     for (int i = 0; i < rowsB; i++)
         for (int j = 0; j < columnsB; j++)
-            inputFileB >> matrixB[i][j];
+            fscanf(inputB, "%d", &matrixB[i][j]);
 
-    inputFileA.close();
-    inputFileB.close();
+    fclose(inputA);
+    fclose(inputB);
 
     if (columnsA != rowsB) {
-        outputFile << "Error: Number of columns in the first matrix must be equal to the number of rows in the second matrix." << endl;
-        outputFile.close();
+        fprintf(outputFile, "Error: Number of columns in the first matrix must be equal to the number of rows in the second matrix.\n");
+        fclose(outputFile);
         return;
     }
 
     vector<vector<int>> matrixC(rowsA, vector<int>(columnsB));
-    int buffer = 0;
-
-    for (int i = 0; i < rowsA; i++) {
-        for (int j = 0; j < columnsB; j++) {
-            for (int k = 0; k < columnsA; k++) {
-                buffer += matrixA[i][k] * matrixB[k][j];
-            }
-            matrixC[i][j] = buffer;
-            buffer = 0;
-        }
-    }
-
-    for (int i = 0; i < rowsA; i++) {
+    for (int i = 0; i < rowsA; i++)
         for (int j = 0; j < columnsB; j++)
-            outputFile << matrixC[i][j] << " ";
-        outputFile << endl;
+            for (int k = 0; k < columnsA; k++)
+                matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+
+    for (const auto& row : matrixC) {
+        for (const auto& val : row)
+            fprintf(outputFile, "%d ", val);
+        fprintf(outputFile, "\n");
     }
+
+    fclose(outputFile);
 }
 
 
 void task1_2() {
-    ifstream inputFile("tests/input1_2.txt");
-    ofstream outputFile("tests/output1_2.txt");
+    FILE* inputFile = fopen("tests/input1_2.txt", "r");
+    FILE* outputFile = fopen("tests/output1_2.txt", "w");
 
-    int k;
     vector<int> numbers;
+    int num;
 
-    while (inputFile >> k)
-        numbers.push_back(k);
+    while (fscanf(inputFile, "%d", &num) == 1)
+        numbers.push_back(num);
 
-    inputFile.close();
+    fclose(inputFile);
+
     cout << "Input number k: ";
+    int k;
     cin >> k;
 
-    for (auto num : numbers)
-        if (num % k == 0)
-            outputFile << num << " ";
+    for (const auto& val : numbers)
+        if (val % k == 0)
+            fprintf(outputFile, "%d ", val);
 
-    outputFile.close();
+    fclose(outputFile);
 }
 
 
 int main() {
-    ios::sync_with_stdio(false);
     task8_1();
     task8_2();
     task11_1();
