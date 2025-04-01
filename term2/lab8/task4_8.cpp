50888c9868384ef5c3226671dfb2fe77844d8a72
456i <timofeymarushko@gmail.com>
2025-04-01 20:03:41 +0300

    modified lab8

--- term2/lab8/task4_8.cpp
+++ term2/lab8/task4_8.cpp
@@ -1,77 +1,36 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include <limits>
+
 using namespace std;
 
 double f(double x) {
     return x * x * x + x * x - 2 * x - 2;
 }
 
-double dichotomy(double (*f)(double), double a, double b, double e) {
-    double c;
-    while ((b - a) / 2.0 > e) {
-        c = (a + b) / 2.0;
-        if (f(c) == 0.0)
-            return c;
-        if (f(a) * f(c) < 0)
-            b = c;
-        else
-            a = c;
-    }
-    return (a + b) / 2.0;
-}
+double dichotomy(double (*func)(double), double a, double b, double e) {
+    double c = (a + b) / 2.0;
 
-vector<double> findAllRoots(double (*f)(double), double a, double b, double e, int subdivisions = 1000) {
-    vector<double> roots;
-    double step = (b - a) / subdivisions;
-    double x1 = a;
+    if (b - a <= 2 * e) return c;
 
-    for (int i = 0; i < subdivisions; ++i) {
-        double x2 = x1 + step;
-        if (abs(f(x1)) < e) {
-            if (roots.empty() || abs(roots.back() - x1) > e)
-                roots.push_back(x1);
-        }
-        if (f(x1) * f(x2) < 0) {
-            double root = dichotomy(f, x1, x2, e);
-            bool alreadyFound = false;
-            for (double r : roots) {
-                if (abs(r - root) < e) {
-                    alreadyFound = true;
-                    break;
-                }
-            }
-            if (!alreadyFound)
-                roots.push_back(root);
-        }
-        x1 = x2;
-    }
-
-    if (abs(f(b)) < e) {
-        if (roots.empty() || abs(roots.back() - b) > e)
-            roots.push_back(b);
-    }
-
-    return roots;
+    if (func(a) * func(c) < 0)
+        return dichotomy(func, a, c, e);
+    else
+        return dichotomy(func, c, b, e);
 }
 
 int main() {
-    double a = -100.0;
-    double b = 100.0;
-    double e = 0.00001;
+    double a = -2.0;
+    double b = 2.0;
+    double e = 1e-6;
 
-    vector<double> roots = findAllRoots(f, a, b, e);
+    if (f(a) * f(b) >= 0) {
+        cout << "f(a)*(b)<0" << endl;
+        return 1;
+    }
 
-    if (roots.empty()) {
-        cout << "Roots weren't found." << endl;
-    }
-    else {
-        cout << "Found roots:" << endl;
-        for (double root : roots) {
-            cout << root << endl;
-        }
-    }
+    double root = dichotomy(f, a, b, e);
+    cout << "Found root: " << root << endl;
+    cout << "Function value at root: " << f(root) << endl;
 
     return 0;
 }
